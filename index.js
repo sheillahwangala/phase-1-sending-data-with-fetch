615d644fc773282fc40de22774793f4746cea686
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-08-26 18:54:36 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+function submitData(name, email) {
+
+    let data = {
+        name: name,
+        email: email
+    };
+
+    let userData = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(data),
+    };
+
+
+    return fetch('http://localhost:3000/users', userData)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            document.body.innerHTML = object["id"];
+            console.log(object);
+        })
+        .catch(function (error) {
+            document.body.innerHTML = error.message
+        })
+}
+
+
+
+
